5041a582692033c50c8c7d5ab3427d674d716b55
janl0 <janlin1805@gmail.com>
2024-09-02 21:30:10 +0200

    Test zu kürzlichen gerichten

---
+++ app/src/main/java/com/application/mykitchen/consumables/Beverage.java
@@ -0,0 +1,41 @@
+package com.application.mykitchen.consumables;
+
+/**
+ * @author Jan Lindauer
+ */
+public class Beverage extends Consumable {
+    private Integer volume;
+    private Integer alcoholVol;
+
+    /**
+     *
+     * @return die Menge in Milliliter
+     */
+    public Integer getVolume() {
+        return volume;
+    }
+
+    /**
+     *
+     * @param volume Menge in Milliliter
+     */
+    public void setVolume(Integer volume) {
+        this.volume = volume;
+    }
+
+    /**
+     *
+     * @return den Alkoholprozentsatz
+     */
+    public Integer getAlcoholVol() {
+        return alcoholVol;
+    }
+
+    /**
+     *
+     * @param alcoholVol den Alkoholprozentsatz
+     */
+    public void setAlcoholVol(Integer alcoholVol) {
+        this.alcoholVol = alcoholVol;
+    }
+}
--- app/src/main/java/com/application/mykitchen/consumebles/Beverage.java
+++
@@ -1,8 +0,0 @@
-package com.application.mykitchen.consumebles;
-
-public class Beverage {
-    private Integer volume;
-    private String name;
-    private String surname;
-    private Integer alcoholVol;
-}
